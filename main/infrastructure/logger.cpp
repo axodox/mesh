40810a15f86319a7067d2017c909176c1125ffbe
Peter Major <peter.major@cae.com>
2021-07-11 15:57:30 +0200

    Added time to logger

--- main/infrastructure/logger.cpp
+++ main/infrastructure/logger.cpp
@@ -1,6 +1,8 @@
 #include "logger.hpp"
 #include "text.hpp"
 
+using namespace std;
+
 namespace mesh::infrastructure
 {
   const char* to_string(log_severity severity)
@@ -22,11 +24,25 @@ namespace mesh::infrastructure
     }
   }
 
+  std::string get_timestamp()
+  {
+    time_t utc_now;
+    time(&utc_now);
+
+    tm local_now;
+    localtime_r(&utc_now, &local_now);
+
+    string time_as_text(9, '\0');
+    strftime(time_as_text.data(), time_as_text.size(), "%T", &local_now);
+
+    return time_as_text;
+  }
+
   void log_message(log_severity severity, const char *format, ...)
   {
     va_list args;
     va_start(args, format);
-    auto text = format_text("= %s >> %s\n", to_string(severity), format_text(format, args).c_str());
+    auto text = format_text("%s [%s] main: %s\n", get_timestamp().c_str(), to_string(severity), format_text(format, args).c_str());
     va_end(args);
     printf(text.c_str());
   }
@@ -35,7 +51,7 @@ namespace mesh::infrastructure
   {
     va_list args;
     va_start(args, format);
-    auto text = format_text("= %s / %s >> %s\n", to_string(severity), _name, format_text(format, args).c_str());
+    auto text = format_text("%s [%s] %s: %s\n", get_timestamp().c_str(), to_string(severity), _name, format_text(format, args).c_str());
     va_end(args);
     printf(text.c_str());
   }
